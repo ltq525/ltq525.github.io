b9ed758440ed5867ce65681ae60e8c706b34cfb1
quann <1766445410@qq.com>
2024-02-06 01:54:36 +0800

    update

--- git.sh
+++ git.sh
@@ -1,13 +1,13 @@
-#! /bin/bash
+#! /bin/sh
 
-read -p "是否运行git批命令(Y/n)? " op
+read -p "是否运行git命令(Y/n)? " op
 
-if [ op eq "y" or op eq "Y" ]
-do
-read -p "请输入commit的名称:  " name
-mkdocs build -c
-git add .
-git commit -m "$name"
-git push
-done
+if [ $op == "y" ] || [ $op == "Y" ]
+then
+    read -p "请输入commit的名称:  " name
+    mkdocs build -c
+    git add .
+    git commit -m "$name"
+    git push
+fi
 
